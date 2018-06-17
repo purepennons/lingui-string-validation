b0c8605a7afc706ffc5b15e26978f2aa2c727ec7
Chiahao Lin <y80811@gmail.com>
2018-06-17 11:33:33 +0800

    feat: `getTagsTree` function works

--- __test__/validation.spec.js
+++ __test__/validation.spec.js
@@ -1,4 +1,8 @@
-import { getVariablesArray, isValidParanthesesPairs } from '../src/';
+import {
+  getVariablesArray,
+  isValidParanthesesPairs,
+  getTagsTree
+} from '../src/';
 
 describe('getVariablesArray', () => {
   it('should only parse parantheses with a variable name', () => {
@@ -34,12 +38,54 @@ describe('isValidParanthesesPairs', () => {
     expect(isValidParanthesesPairs('}prefix{}{center}{}postfix{')).toBe(false);
 
     // contain uncorrect order
-    expect(isValidParanthesesPairs('prefix{}{}center}{{}{}postfix')).toBe(false);
+    expect(isValidParanthesesPairs('prefix{}{}center}{{}{}postfix')).toBe(
+      false
+    );
 
     // contain nested pairs
     expect(isValidParanthesesPairs('prefix{}{{center}}{}postfix')).toBe(false);
 
     // unpaired
     expect(isValidParanthesesPairs('prefix{}{center{}postfix')).toBe(false);
+  });
+});
+
+describe('getTagsTree', () => {
+  it('should parse string to a tree', () => {
+    const str = `prefix<0>aaa<2>bbb</2></0>center<1><5>ddd</5><3><4>some text with {variable}</4><6>ccc</6></3></1>postfix`;
+
+    expect(getTagsTree(str)).toEqual([
+      {
+        tag: '0',
+        children: [
+          {
+            tag: '2',
+            children: []
+          }
+        ]
+      },
+      {
+        tag: '1',
+        children: [
+          {
+            tag: '5',
+            children: []
+          },
+          {
+            tag: '3',
+            children: [
+              {
+                tag: '4',
+                children: []
+              },
+              {
+                tag: '6',
+                children: []
+              }
+            ]
+          }
+        ]
+      }
+    ]);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-import { isString } from 'lodash'
+import { isString } from 'lodash';
 
 export function getVariablesArray(str) {
-  if (!isString(str)) return false
-  return str.match(/\{[A-Za-z_][A-Za-z0-9_]*\}/g) || []
+  if (!isString(str)) return false;
+  return str.match(/\{[A-Za-z_][A-Za-z0-9_]*\}/g) || [];
 }
 
 export function isValidParanthesesPairs(str) {
-  if (!isString(str)) return false
+  if (!isString(str)) return false;
   const result = str.match(/\{|\}/g).reduce(
     (acc, v, idx, arr) => {
-      if (acc.done) return acc
+      if (acc.done) return acc;
 
       switch (v) {
         case '{':
           if (acc.prev !== '' || idx === arr.length - 1) {
-            acc.result = false
-            acc.done = true
+            acc.result = false;
+            acc.done = true;
           }
-          acc.prev = '{'
-          break
+          acc.prev = '{';
+          break;
         case '}':
           if (acc.prev !== '{') {
-            acc.result = false
-            acc.done = true
+            acc.result = false;
+            acc.done = true;
           }
-          acc.prev = ''
-          break
+          acc.prev = '';
+          break;
         default:
-          acc.result = false
-          acc.done = true
+          acc.result = false;
+          acc.done = true;
       }
-      return acc
+      return acc;
     },
-    { prev: '', result: true, done: false },
-  )
-  return result.result
+    { prev: '', result: true, done: false }
+  );
+  return result.result;
+}
+
+// /<(\s*(\d+)[^>]*)>(.*?)<\s*\/\s*\1>/g
+// /<(\s*(\d+)[^>]*)>(.*?)<\s*\/\s*\1>/g
+export function getTagsTree(str) {
+  if (!isString(str)) return [];
+  const tree = [];
+  const regx = /<(\s*(\d+)[^>]*)>(.*?)<\s*\/\s*\1>/g;
+  let m = null;
+  while ((m = regx.exec(str))) {
+    const tag = {
+      tag: m[1],
+      children: getTagsTree(m[3])
+    };
+    tree.push(tag);
+  }
+  return tree;
 }
